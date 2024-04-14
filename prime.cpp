bc5e5bc44f8be30796721ea49ed0544f88da0db2
techlojutt <muhammadshoaib2140jutt@gmail.com>
2024-04-15 00:39:38 +0500

    first commit

---
+++ prime.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cin >> n ;
+    int i = 2;
+    while (i<n)
+    {
+        if(n%i==0){
+            cout << "Not prime for " << i << endl ;
+        }
+        else{
+            cout<<"prime for "<<i<<endl;
+        }
+        i=i+1;
+       
+    }
+
+            
+    
+}
